ec8c495d5dd4df0cf1e6057d2924b093b80f6751
vinberg88 <vinberg88@example.com>
2025-12-10 08:19:45 +0100

    Make Node.js guide the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-import Hero from '@/components/Hero'
-import Features from '@/components/Features'
-import QuickStart from '@/components/QuickStart'
-import TechStack from '@/components/TechStack'
-import Community from '@/components/Community'
+import NodePage from './node/page'
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <Features />
-      <QuickStart />
-      <TechStack />
-      <Community />
-    </>
-  )
+  return <NodePage />
 }
